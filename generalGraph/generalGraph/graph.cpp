e33e9c6a53a44e8b7547254bda50eb2d0a43d75d
Tomas Karella <tomas.karella@gmail.com>
2017-04-24 16:06:32 +0200

    Squashed commit of the following:
    commit 7625059b4f01d7f7168a97a76b527c7babaa0aa2
    Author: Tomas Karella <tomas.karella@gmail.com>
    Date:   Mon Apr 24 16:02:41 2017 +0200
    
        Availability test add
    
    commit e22ed59fd4e3d08343943ed7cb16c07f26994352
    Author: Tomas Karella <tomas.karella@gmail.com>
    Date:   Mon Apr 24 15:38:41 2017 +0200
    
        Cycle testing algorithm
    
    commit a392d74624a4b517a1ce1cedcf78ae48e79ef02f
    Author: Tomas Karella <tomas.karella@gmail.com>
    Date:   Mon Apr 10 10:54:58 2017 +0200
    
        find added(do not mark visited verticies
    
    commit 9de1392bc49ced81bc80dc9219b8cbc041281d12
    Author: Tomas Karella <tomas.karella@gmail.com>
    Date:   Fri Apr 7 15:18:01 2017 +0200
    
        from edges

--- generalGraph/generalGraph/graph.cpp
+++ generalGraph/generalGraph/graph.cpp
@@ -1,26 +1,42 @@
 #include "graph.h"
 #include <string>
 #include <iostream>
+#include <set>
 using namespace  std;
 int main() {
-
-	Graph<int, double> g;
-	auto* a = g.add_vertex(1);
-	auto* b = g.add_vertex(2);
-	auto* c = g.add_vertex(3);
-	auto* f = g.connect(c, a, 3.25);
-	auto* e = g.connect(a, b, 3.14);
-
 	
-	for (auto&& i : g.edges)
-		cout << i->from->value << " --" << i->value << "--> " << i->to->value << endl;
-	cout << endl; 
-	g.disconnect(f);
-	for (auto&& i : g.edges)
-		cout << i->from->value << " --" << i->value << "--> " << i->to->value << endl;
+	Graph<unique_ptr<int>, unique_ptr<double>> g;
+	auto* a = g.add_vertex(make_unique<int>(1));
+	auto* b = g.add_vertex(make_unique<int>(2));
+	auto* c = g.add_vertex(make_unique<int>(3));
+	auto* d = g.add_vertex(make_unique<int>(4));
+	auto* e = g.add_vertex(make_unique<int>(5));
+	auto* f = g.add_vertex(make_unique<int>(6));
 
-	string blank;
-		getline(cin, blank);
+	g.connect(a, c, make_unique<double>(1.0));
+	g.connect(b, d, make_unique<double>(2.0));
+	g.connect(c, d, make_unique<double>(3.0));
+	g.connect(c, e, make_unique<double>(4.0));
+	g.connect(c, f, make_unique<double>(5.0));
+	g.connect(d, f, make_unique<double>(6.0));
+	g.connect(f, c, make_unique<double>(7.0));
+
+	if (g.cycle_detection())
+		cout << "true" << endl;
+	else
+		cout << "false" << endl;
+
+	vector<Vertex<unique_ptr<int>>*> from;
+	from.push_back(a);
+	from.push_back(b);
+	if (g.all_vertices_available_from(from))
+		cout << "true";
+	else
+		cout << "false";
+
+
+	std::string blank; 
+	getline(cin, blank);
 
 	return 0;
 }
